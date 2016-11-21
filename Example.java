8965a874fdc67ac91dc8c03492961c012daf26eb
DivyaVipin <divyababu1001@gmail.com>
2016-11-21 16:59:26 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,15 @@
+
+public class Example {
+	int x; int y;
+	String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example pnt = new Example();
+		System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+
+	}
+
+}
+/*Output
+ * -----
+ * pnt is null 0 0*/
